ee02bd4f135084b1a4f083b1419af9eeb15b275e
Aya <an7099265@gmail.com>
2025-12-03 00:21:31 +0100

    I created new LWC file

--- force-app/main/default/lwc/lWCTest/lWCTest.js
+++ force-app/main/default/lwc/lWCTest/lWCTest.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class LWCTest extends LightningElement {}
+export default class LWCTest extends LightningElement {
+    //this is test lwc
+}
